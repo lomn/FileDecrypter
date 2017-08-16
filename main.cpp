2ebfa4d8627940d8bd265c55bcbc99834b15656f
lomn <31063427+lomn@users.noreply.github.com>
2017-08-16 14:29:04 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,19 +14,29 @@ int main(int argc,char** argv)
 	char path[128];
 	FILE *in, *out;
 
+	//Only one argument needed
 	if(argc != 2){
 		printf("[-] Usage : %s <file>\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
+	//prevent overflow :D
+	if(strlen(argv[1])>=128){
+		printf("[-] Path is too long");
+		return EXIT_FAILURE;
+	}
+
+	//copy the path with extension of the output
 	snprintf(path, sizeof(path)-1, "%s.bmp", argv[1]);
 
+	//Check for error while opening the source file
 	in = fopen(argv[1], "r");
 	if(in == NULL){
 		printf("[-] Error while openning : %s\n", argv[1]);
 		return EXIT_FAILURE;
 	}
 
+	//Check for error while opening the output file
 	out = fopen(path, "w");
 	if(out == NULL){
 		printf("[-] Error while openning : %s \n\n", path);
@@ -39,13 +49,13 @@ int main(int argc,char** argv)
 }
 
 void decrypt_main (FILE *in,FILE *out){
-	long int key = 0x0; //0xdeadbeef
+	long int key = 0xa12c3f; //0xdeadbeef
 	printf("[+] Decrypting\n");
 	
-	while(key < 0xffffff){
+	//while(key < 0xffffff){
 		decrypt_file(in, out, key);
 		key++;
-	}
+	//}
 	return;
 }
 
@@ -54,6 +64,7 @@ void decrypt_buffer (unsigned char* buffer, size_t size, long int key)
 	size_t i = 0;
 	int j;
 
+	//Simply XOR the buffer with the key which is 32 bytes long
 	for(i=0; i < size; i++){
 		buffer[i] ^= key;
 	}
@@ -63,10 +74,14 @@ void decrypt_file(FILE* in, FILE* out, long int key){
 	unsigned char buffer[BUFF_SIZE];
 	unsigned char* pbuffer = buffer;
 	size_t size;
+	int counter=1;
 
+	//in this order, read the file and copy it to the buffer, decrypt the buffer and then write it to the output file
 	do {
     size = fread(buffer, 1, BUFF_SIZE, in);
     decrypt_buffer(pbuffer, size, key);
     fwrite(buffer, 1, size, out);
+    counter++;
   }while(size == BUFF_SIZE);
+  printf("[-_-] Buffer filled for : %d times \n", counter);
 }
